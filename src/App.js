1fff06e97a9920c111790c84882896e348e8f3ea
Ino Tech <ezekieldavid150@gmail.com>
2026-01-12 00:33:59 +0100

    Fix initialItems structure and update Form component layout

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ const initialItems = [
   { id: 2, 
     description: "Socks", 
     quantity: 12, 
+    packed: true 
+  },
+  { id: 3, 
+    description: "Chairs", 
+    quantity: 1, 
     packed: false 
-  }
+  },
 ];
 
 export default function App() {
@@ -29,14 +34,36 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
-      <h3>What do you nedd for your trip</h3>
-    </div>
+    <form className="add-form">
+      <h3>What do you need for your trip</h3>
+      <select>
+        {Array.from({ length: 20}, (_, i) => i + 1).map((num) => (
+        <option value={num} key={num}>
+          {num}
+        </option>
+  ))}
+      </select>
+      <input type="text" placeholder="item.." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingLists() {
-  return <div className="list">Lists</div>;
+  return (
+    <div className="list">
+      <ul>{initialItems.map(item=><Item item={item} />)}</ul>
+    </div>
+  );
+}
+
+function Item({item}) {
+  return (
+  <li>
+    <span style={item.packed ? {textDecoration:"line-through"} : {}}>{item.quantity} {item.description}</span>
+    <button>❌</button>
+  </li>
+);
 }
 
 function Stats() {

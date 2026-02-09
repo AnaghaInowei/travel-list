0626c8a64ee7af9b623c07a413c7274b262d52f7
Ino Tech <ezekieldavid150@gmail.com>
2026-02-09 22:42:25 +0100

    Refactor App and PackingLists components to handle item deletion and toggling, and remove initialItems array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  {
-    id: 1,
-    description: "Passports",
-    quantity: 2,
-    packed: false,
-  },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Chairs", quantity: 1, packed: false },
-];
-
 export default function App() {
 
   const [items, setItems] = useState([]);
@@ -19,11 +8,19 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function  handleToggleItem(id) {
+    setItems((items) => items.map((item) => item.id === id ? {...item, packed: !item.packed} : item));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingLists items={items}/>
+      <PackingLists items={items} onDeleteItem={handleDeleteItem} onToggleItems={handleToggleItem}/>
       <Stats />
     </div>
   );
@@ -72,25 +69,26 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingLists({ items }) {
+function PackingLists({ items, onDeleteItem, onToggleItems }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} onToggleItems={onToggleItems} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItems }) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItems(item.id)}/>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }

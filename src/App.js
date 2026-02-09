152cd198003c4fc962cd258a40d23815dc8b2543
Ino Tech <ezekieldavid150@gmail.com>
2026-02-10 00:43:15 +0100

    Add sorting functionality to PackingLists based on input, description, and packed status

--- src/App.js
+++ src/App.js
@@ -72,11 +72,20 @@ function Form({onAddItems}) {
 function PackingLists({ items, onDeleteItem, onToggleItems }) {
   const [sortBy, setSortBy] = useState("input");
 
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed") sortedItems =  items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
-          <Item item={item} 
+        {sortedItems.map((item) => (
+          <Item 
+          item={item} 
           onDeleteItem={onDeleteItem} 
           onToggleItems={onToggleItems} 
           key={item.id} />
